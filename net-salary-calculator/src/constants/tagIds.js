78daae3088ed290de211c880de71c5cb03ca2372
Gustavo Lemes <gustavolemes997@gmail.com>
2022-03-14 14:16:22 -0300

    Create Header Component test

---
+++ net-salary-calculator/src/constants/tagIds.js
@@ -0,0 +1,7 @@
+const HEADER_TEXT_ID = 'header-text';
+const HEADER_LOGO_ID = 'header-logo';
+
+module.exports = {
+  HEADER_TEXT_ID,
+  HEADER_LOGO_ID,
+};
---
+++ net-salary-calculator/src/tests/header.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen, configure } from '@testing-library/react';
+
+import Header from '../components/Header';
+
+import {
+  HEADER_TEXT_ID,
+  HEADER_LOGO_ID,
+} from '../constants/tagIds';
+
+configure({ testIdAttribute: 'id' });
+
+describe('Create a Header with the following characteristics', () => {
+  test('Verify if the text "Calculadora de salário líquido" to be in the header', () => {
+    render(<Header />);
+    const text = screen.getByTestId(HEADER_TEXT_ID);
+
+    expect(text).toBeInTheDocument();
+    expect(text).toHaveTextContent('Calculadora de salário líquido');
+  });
+
+  test('Verify if the logo exists in the header', () => {
+    render(<Header />);
+    const logo = screen.getByTestId(HEADER_LOGO_ID);
+
+    expect(logo).toBeInTheDocument();
+  });
+});
